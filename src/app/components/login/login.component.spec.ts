171ec4c99525b5a27b806c25187e6bf3fda4969c
Juan Pablo Muñoz <munozsjp@gmail.com>
2022-10-23 16:06:43 +0200

    test: adding unit-test in LoginComponent

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,9 +10,14 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        MatSnackBarModule,
+        RouterTestingModule,
+      ],
+      declarations: [LoginComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +28,27 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('return invalid form', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    const user = component.form.controls['user'];
+    user.setValue('admin');
+    expect(component.form.invalid).toBe(true);
+  });
+
+  it('Debe retornar formulario valido', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+
+    let user = app.form.controls['user'];
+    let password = app.form.controls['password'];
+
+    user.setValue('admin');
+    password.setValue('admin');
+    expect(app.form.invalid).toBeFalse();
   });
 });
